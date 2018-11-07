6fd49d111fb800d3302eb7dacf1ffb3792d4c24d
mischb <mischarberlin@gmail.com>
2018-11-07 17:12:45 -0500

    fixed time division

--- stop-watch/src/components/time.jsx
+++ stop-watch/src/components/time.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 export default function Time(props) {
-  const hundreths = props.clock%100
-  const seconds = (Math.floor(props.clock/(100))%60);
-  const minutes = (Math.floor(props.clock/60**3)%60);
-  const hours = (Math.floor(props.clock/(60**4)%60));
+  const seconds = 100;
+  const minutes = seconds * 60;
+  const hours = minutes * 60;
+  const hundrethsDisplay = props.clock%100
+  const secondsDisplay = (Math.floor(props.clock/(seconds))%60);
+  const minutesDisplay = (Math.floor(props.clock/minutes)%60);
+  const hoursDisplay = (Math.floor(props.clock/hours)%60);
   return(
     <div>
-      <span>{hours} {minutes} {seconds} {hundreths}</span>
+      <span>{hoursDisplay} : {minutesDisplay} : {secondsDisplay} : {hundrethsDisplay}</span>
     </div>
   )
 }
