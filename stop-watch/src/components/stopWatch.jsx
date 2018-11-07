66670336770c6ceca2d1b27c90ec9d6a76453298
Allister Rooke <allisterg@gmail.com>
2018-11-07 13:33:22 -0500

    playing with intervals

--- stop-watch/src/components/stopWatch.jsx
+++ stop-watch/src/components/stopWatch.jsx
@@ -8,13 +8,26 @@ export default class StopWatch extends React.Component{
   constructor(props){
     super(props)
 
+    
+
     this.state = {
-      clock : '00:10:10'
+      clock : 0
     }
+
+    
 
   };
 
   render(){
+    let time = this.state.clock
+    setInterval(() => 
+    
+    { time++
+      this.setState( {
+      clock: time
+    })
+    }, 1000)
+
     return (
       <div>
         <Time clock={this.state.clock}/>
